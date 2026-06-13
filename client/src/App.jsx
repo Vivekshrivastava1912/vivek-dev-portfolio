fd45caf3810c34854fa1398c588369e06779957f
Vivekshrivastava1912 <shrivastavavivek19d02005@gmail.com>
2026-06-13 10:55:47 +0530

    initial-setup

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+    <h1>hello bhai naw portfolio bana ra ahu 
+
+    </h1>
+    </>
+  )
+}
+
+export default App
